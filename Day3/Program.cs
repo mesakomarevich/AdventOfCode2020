854ef3f5a4d92ea0c5bd86c9fa4c3585c4b21bbe
Mesa Komarevich <mesa@attabotics.com>
2020-12-04 16:30:52 -0700

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+            var map = lines.Select(x => x.ToCharArray());
+        }
+
+        static void Part1(char[,] map)
+        {
+            
+        }
+    }
+}
