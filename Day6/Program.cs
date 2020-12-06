8d77a515b6dc64e33409d178ba49806acce4f679
Mesa Komarevich <mesakom@gmail.com>
2020-12-06 03:57:01 -0700

    Day6

---
+++ Day6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var groups = (await File.ReadAllTextAsync("input.txt")).Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+            Part1(groups);
+            Part2(groups);
+        }
+
+        static void Part1(string[] groups)
+        {
+            Console.WriteLine(groups.Sum(x => x.Replace("\n", "").Distinct().Count()));
+        }
+
+        static void Part2(string[] groups)
+        {
+            Console.WriteLine(groups.Sum(x => x.Split("\n").Aggregate((curr, next) => new string(curr.Intersect(next).ToArray())).Length));
+        }
+    }
+}
